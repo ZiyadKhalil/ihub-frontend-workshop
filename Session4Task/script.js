00a1e9252b53d22426be80ca9164295add78b452
ZiyadKhalil <ziyadhkhalil@gmail.com>
2018-07-04 00:54:06 +0200

    edit

--- Session4Task/script.js
+++ Session4Task/script.js
@@ -4,14 +4,21 @@ var myButton = document.getElementById('myButton');
 var successAlert = document.getElementById('successAlert');
 var failAlert = document.getElementById('failAlert');
 var values = myList.innerHTML;   //var to store list items
+var valuess = ["1","2","3","4","5"];
+for(var i =0 ;i<myList.children;i++)
+    valuess.push(myListChildren[i].textContent);
 myInput.addEventListener('input',onChangeFunction);
 myButton.addEventListener('click',onButtonClickedFunction);
 
 function onChangeFunction() {
+  console.log(myInput.value);
+  console.log(values);
+  console.log(myList.children);
+  console.log(valuess);
     var tempInnerHTML = '';             //A Temporary String to hold all values starts with the input
-    for(var i=0;i<myList.children.length;i++){
-      if(myList.children[i].textContent.startsWith(myInput.value))
-          tempInnerHTML+=`<li class="list-group-item">${myList.children[i].textContent}</li> \n`;
+    for(var i=0;i<valuess.length;i++){
+      if(valuess[i].startsWith(myInput.value))
+          tempInnerHTML+=`<li class="list-group-item">${valuess[i]}</li> \n`;
     }
     myList.innerHTML=tempInnerHTML;
     if(myInput.value=="")               // to return to default items if input is empty
@@ -27,6 +34,7 @@ function onButtonClickedFunction() {
     }
     else {
       values+=`\n<li class="list-group-item">${myInput.value}</li>`;
+      valuess.push(myInput.value);
       myInput.value='';
       failAlert.style.display='none';
       failAlert.style.visibility='hidden';
