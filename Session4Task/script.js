36dce494f95e58ef5292f43410c3a8b1aba88c26
ZiyadKhalil <ziyadhkhalil@gmail.com>
2018-07-04 01:03:27 +0200

    anotherApproach

--- Session4Task/script.js
+++ Session4Task/script.js
@@ -3,38 +3,28 @@ var myInput = document.getElementById('myInput');
 var myButton = document.getElementById('myButton');
 var successAlert = document.getElementById('successAlert');
 var failAlert = document.getElementById('failAlert');
-var values = myList.innerHTML;   //var to store list items
-var valuess = ["1","2","3","4","5"];
-for(var i =0 ;i<myList.children;i++)
-    valuess.push(myListChildren[i].textContent);
 myInput.addEventListener('input',onChangeFunction);
 myButton.addEventListener('click',onButtonClickedFunction);
 
 function onChangeFunction() {
-  console.log(myInput.value);
-  console.log(values);
-  console.log(myList.children);
-  console.log(valuess);
-    var tempInnerHTML = '';             //A Temporary String to hold all values starts with the input
-    for(var i=0;i<valuess.length;i++){
-      if(valuess[i].startsWith(myInput.value))
-          tempInnerHTML+=`<li class="list-group-item">${valuess[i]}</li> \n`;
+    for(var i=0;i<myList.children.length;i++){
+      if(!myList.children[i].textContent.startsWith(myInput.value))
+          myList.children[i].style.display='none';
+      else {
+            myList.children[i].style.display='block';
+          }
     }
-    myList.innerHTML=tempInnerHTML;
-    if(myInput.value=="")               // to return to default items if input is empty
-          myList.innerHTML=values;
 
 }
 function onButtonClickedFunction() {
     if(myInput.value==''){
-      failAlert.style.display='block';
-      failAlert.style.visibility='visible';
+      failAlert.style.display='block';              // For Alerts
+      failAlert.style.visibility='visible';         //
       successAlert.style.display='none';
       successAlert.style.visibility ='hidden';
     }
     else {
-      values+=`\n<li class="list-group-item">${myInput.value}</li>`;
-      valuess.push(myInput.value);
+      myList.innerHTML+=`\n<li class="list-group-item">${myInput.value}</li>`;
       myInput.value='';
       failAlert.style.display='none';
       failAlert.style.visibility='hidden';
